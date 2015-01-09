24ff35aa5c251599accfd6184c8d821feccb78c8
Victor Grishchenko <victor.grishchenko@gmail.com>
2015-01-09 22:42:23 +0500

    Initial Commit

---
+++ bat/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  bat
+//
+//  Created by Victor Grishchenko on 09.01.15.
+//  Copyright (c) 2015 Victor Grishchenko. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
